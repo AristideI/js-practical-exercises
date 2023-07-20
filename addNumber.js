93e03cac3d28c5dcffcf489f6f587c04810bab9e
AristideI <a.isingizwe@alustudent.com>
2023-07-20 10:10:44 +0200

    Latest Update

--- addNumber.js
+++ addNumber.js
@@ -1,3 +1,27 @@
 function addNumbers(str) {
     let pro = str.split("")
+    if(pro.includes("/")){
+        let k = Number(pro[pro.indexOf("/") - 1]) / Number(pro[pro.indexOf("/") + 1])
+        pro.splice(pro.indexOf("/") - 1, 3, k)
+      
+    }
+    if(pro.includes("*")){
+        let k = Number(pro[pro.indexOf("*") - 1]) * Number(pro[pro.indexOf("*") + 1])
+        pro.splice(pro.indexOf("*") - 1, 3, k)
+       
+    }
+    if(pro.includes("+")){
+        let k = Number(pro[pro.indexOf("+") - 1]) + Number(pro[pro.indexOf("+") + 1])
+        pro.splice(pro.indexOf("+") - 1, 3, k)
+       
+    }
+    if(pro.includes("-")){
+        let k = Number(pro[pro.indexOf("-") - 1]) - Number(pro[pro.indexOf("-") + 1])
+        pro.splice(pro.indexOf("-") - 1, 3, k)
+        
+    }
+
+    return Number(pro.join(""))
 }
+
+console.log(addNumbers("3+5*3/3"))
